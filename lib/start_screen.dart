b484fa1de2904e9bd71124643e6c1358a2bd883a
Kishna Bansal <kb.20051981@gmail.com>
2023-12-17 00:21:29 +0530

    Added an icon button and learned about opacity widget and its alternative

--- lib/start_screen.dart
+++ lib/start_screen.dart
@@ -9,21 +9,30 @@ class StartScreen extends StatelessWidget {
       child: Column(
         mainAxisSize: MainAxisSize.min,
         children: [
-          Image.asset('assets/images/quiz-logo.png',
-                      width: 300,
-          ),
+            Image.asset('assets/images/quiz-logo.png',
+                        width: 300,
+                        color: Color.fromARGB(151, 255, 255, 255),
+            ),
+          //Using opacity widget is not suggested as it is performance effective in complex apps 
+          // Opacity(
+          //   opacity: 0.6,
+          //   child: Image.asset('assets/images/quiz-logo.png',
+          //               width: 300,
+          //   ),
+          // ),
           const SizedBox(height: 80),
           const Text(
             "Learn Flutter the fun way!!",
             style: TextStyle(fontSize: 24, color: Colors.white),
           ),
           const SizedBox(height: 57),
-          OutlinedButton(
+          OutlinedButton.icon(                          //We connot use child with icon instead we use label there
             onPressed: () {},
             style: OutlinedButton.styleFrom(
               foregroundColor: Colors.white
             ),
-            child: const Text("Start Quiz"),
+            icon: const Icon(Icons.arrow_right_alt),
+            label: const Text("Start Quiz"),
           ),
         ],
       ),
